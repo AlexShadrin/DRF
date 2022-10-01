88fc2c1bc5acfa02e30902d99d37c903896528b2
AlexShadrin <92230248+AlexShadrin@users.noreply.github.com>
2022-10-01 20:01:45 +0400

    lesson_3

--- TODO/authors/serializers.py
+++ TODO/authors/serializers.py
@@ -1,6 +1,17 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
-from .models import Author
-class AuthorModelSerializer(HyperlinkedModelSerializer):
+from rest_framework.serializers import ModelSerializer, HyperlinkedModelSerializer
+from .models import Author, Biographies, Book
+
+class AuthorModelSerializer(ModelSerializer):
     class Meta:
         model = Author
+        fields = '__all__'
+
+class BiographiesHyperlinkedModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Biographies
+        fields = '__all__'
+
+class BookModelSerializer(ModelSerializer):
+    class Meta:
+        model = Book
         fields = '__all__'
