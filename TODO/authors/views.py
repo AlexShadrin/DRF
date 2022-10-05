ba17f7b9ba7f57c445477af6d08ffa44b6583e36
AlexShadrin <92230248+AlexShadrin@users.noreply.github.com>
2022-10-05 15:22:58 +0400

    lesson_4

--- TODO/authors/views.py
+++ TODO/authors/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.pagination import LimitOffsetPagination
 # Create your views here.
 
-
+from authors.filters import BookFilter
 from .models import Author, Biographies, Book
 from .serializers import AuthorModelSerializer, BiographiesHyperlinkedModelSerializer, BookModelSerializer
 
@@ -20,3 +20,18 @@ class BiographiesModelViewSet(ModelViewSet):
 class BookModelViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookModelSerializer
+
+class BookDjangoFilterViewSet(ModelViewSet):
+   queryset = Book.objects.all()
+   serializer_class = BookModelSerializer
+   # filterset_fields = ['id','name']
+   filterset_class = BookFilter
+
+class BookLimitOffsetPagination(LimitOffsetPagination):
+   default_limit = 20
+
+class BookLimitOffsetPaginatonViewSet(ModelViewSet):
+   # renderer_classes = [JSONRenderer]
+   queryset = Book.objects.all()
+   serializer_class = BookModelSerializer
+   pagination_class = BookLimitOffsetPagination
