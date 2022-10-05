f675ba8063745c8453890a228a7a88c6e0ab12b7
AlexShadrin <alex10lord@mail.ru>
2022-10-05 23:02:37 +0400

    lesson_5

--- TODO/frontend/src/App.js
+++ TODO/frontend/src/App.js
@@ -1,34 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import AuthorList from './components/Author.js'
-import axios from 'axios'
+import AuthorList from './components/Author.js';
+import axios from 'axios';
+import BookList from './components/Book.js';
+import {BrowserRouter,Route,Routes,Link,Navigate} from "react-router-dom";
+import NotFound404 from "./components/NotFound404";
+import BooksAuthors from "./components/BooksAuthors";
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            'authors': []
+            'authors': [],
+            'books': [],
         }
     }
     componentDidMount() {
-//        const authors = [
-//            {
-//                'first_name': 'Фёдор',
-//                'last_name': 'Достоевский',
-//                'birthday_year': 1821
-//            },
-//            {
-//                'first_name': 'Александр',
-//                'last_name': 'Грин',
-//                'birthday_year': 1880
-//            },
-//        ]
-//        this.setState(
-//            {
-//                'authors': authors
-//            }
-//        )
+
         axios.get('http://127.0.0.1:8000/api/authors')
             .then(response => {
                 const authors = response.data
@@ -38,12 +27,42 @@ class App extends React.Component {
                     }
                 )
             }).catch(error => console.log(error))
+
+       axios.get('http://127.0.0.1:8000/api/books')
+            .then(response => {
+                const books = response.data
+                    this.setState(
+                    {
+                        'books': books
+                    }
+                )
+            }).catch(error => console.log(error))
     }
 
     render () {
         return (
             <div>
-                <AuthorList authors={this.state.authors} />
+                <BrowserRouter>
+                    <nav>
+                        <li>
+                            <Link to='/'>Authors</Link>
+                        </li>
+                        <li>
+                            <Link to='/books'>Books</Link>
+                        </li>
+                    </nav>
+
+                    <Routes>
+                        <Route exact path='/' element={<Navigate to='/authors'/>}/>
+                        <Route path='/authors'>
+                            <Route index element={<AuthorList authors={this.state.authors}/>}/>
+                            <Route path=':authorId' element={<BooksAuthors books={this.state.books}/>}/>
+                        </Route>
+
+                        <Route exact path ='/books' element={<BookList books={this.state.books}/>} />
+                        <Route path='*' element={<NotFound404/>}/>
+                    </Routes>
+                </BrowserRouter>
             </div>
         )
     }
