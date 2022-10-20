ffb736096cbea90c5ad8255a19303f1a9f6e2964
AlexShadrin <92230248+AlexShadrin@users.noreply.github.com>
2022-10-20 18:41:45 +0400

    lesson_9

--- TODO/TODO/urls.py
+++ TODO/TODO/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,9 +36,34 @@ router.register('book_model_view', BookDjangoFilterViewSet)
 router.register('book_p', BookLimitOffsetPaginatonViewSet)
 from rest_framework.authtoken import views
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='TODO',
+        default_version='v1',
+        description='Documentations',
+        contact=openapi.Contact(email='admi@mail.ru'),
+        license=openapi.License(name='MIT LICENSE'),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
     path('api-token-auth/', views.obtain_auth_token),
+
+    # path('', include('userapp.urls')),
+    path('api/users/v1', include('userapp.urls', namespace='v1')),
+    path('api/users/v2', include('userapp.urls', namespace='v2')),
+
+    path('swagger<str:format>/', schema_view.without_ui()),
+    path('swagger/', schema_view.with_ui('swagger')),
+    path('redoc/', schema_view.with_ui('redoc')),
+
 ]
